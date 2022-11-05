566b39d07f54d6171e33ba3996ee411a4f7ec09b
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-05 10:28:40 +0600

    file added

---
+++ lessons1/index.js
@@ -0,0 +1,1 @@
+document.write("javaScript is my fevourite Language");
---
+++ lessons1/lessons2/script.js
@@ -0,0 +1,15 @@
+// var num = "20";
+//  //num = toString(num);
+// num = parseInt(num);
+// console.log(typeof(num));
+
+var num = 2.3456789
+
+console.log(num.toFixed(1));
+console.log(num.toPrecision(3));
+
+console.log(typeof(Number("12")));
+console.log(typeof(Number("true")));
+
+console.log(Number(true));
+console.log(Number(false));
