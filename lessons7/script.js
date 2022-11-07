a1ad405cd9ba43bf1d5dbf34bb63cda1fd6930ac
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-08 00:05:14 +0600

    added

---
+++ lessons7/script.js
@@ -0,0 +1,31 @@
+// var num = 6;
+// var result = num * num;
+// document.write("The result :"+result +"<br/>");
+
+
+// squire usuing function
+// function squire(num){
+//     var result = num * num;
+//     document.write("The result :"+result +"<br/>");
+// }
+// squire(5);
+// squire(8);
+// squire(12);
+
+//multiply usuing function
+// function squire(num1,num2){
+//     var result = num1 * num2;
+//     document.write("The result :"+result +"<br/>");
+// }
+// squire(5,6);
+// squire(8,9);
+// squire(12,10);
+
+// using return 
+
+function multiply(num1,num2){
+  var result = num1 * num2;
+  return result;
+}
+
+document.write(multiply(2,3))
---
+++ lessons7/script1.js
@@ -0,0 +1,12 @@
+//IIFEs function
+
+// (function display(){
+//     console.log("hello");
+// })();
+
+// sum two number usuing iifes
+
+(function sum(num1,num2){
+    var result = num1+num2;
+    console.log(result);
+})(3,2);
