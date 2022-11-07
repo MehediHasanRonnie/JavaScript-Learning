a88319da5d99de39399c9789c856106546810225
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-07 22:25:57 +0600

    study commit

---
+++ lessons6/script.js
@@ -0,0 +1,14 @@
+
+// for(let i = 0; i<=100; i++){
+//     if(i == 10){
+//         break;
+//     }
+//     document.write(" "+i);
+// }
+
+// for(let i = 0; i<=100; i++){
+//     if(i %2 == 0){
+//         continue;
+//     }
+//     document.write(" "+i);
+// }
---
+++ lessons6/tarnaryOperator.js
@@ -0,0 +1,10 @@
+// var number = Number(prompt("enter the number:"));
+
+// var result = number > 0? "positive" : "Negetive";
+
+// document.write(result);
+
+let number = Number(prompt("Enter the number:"))
+ var result = number > 0? "positive" : number < 0? "negetive" : "Zero";
+
+ console.log(result);
