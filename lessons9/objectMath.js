d670793db08d7ee648c6176b557a2809fa37901a
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-09 12:16:06 +0600

    math commmited

---
+++ lessons9/objectMath.js
@@ -0,0 +1,7 @@
+//INPUT NUMBER FROM USER AND FIND THE MAXIMUM NUMBER
+var num1 = parseInt(prompt("entetr the first number:"));
+var num2 = parseInt(prompt("entetr the second number:"));
+var num3 = parseInt(prompt("entetr the third number:"));
+
+var maximum = Math.max(num1,num2,num3);
+console.log(maximum);
