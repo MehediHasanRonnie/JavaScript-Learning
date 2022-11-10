589b7cfc1bb3137942cd2efd40b491dda75a8b05
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-10 11:19:28 +0600

    commited

---
+++ lessons10/index1.js
@@ -0,0 +1,4 @@
+document.querySelector("a").innerHTML = "Main";
+document.querySelector("li a").innerHTML = "head";
+document.querySelector("#li2").innerHTML = "body";
+document.querySelector(".my-div a").innerHTML = "Info";
