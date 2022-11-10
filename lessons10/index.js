3a4cad7cb833c630f01bb82161d31862eae54d64
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-10 10:31:31 +0600

    commited

---
+++ lessons10/index.js
@@ -0,0 +1,17 @@
+// getElementsById
+
+// document.getElementById("heading").innerHTML = "Hello";
+
+// var love = document.getElementById("pera");
+// love.innerHTML = "bye";
+
+//getElementsByTagName
+//document.getElementsByTagName("h1")[1].innerHTML = "Mehedi"
+
+//getElementsByClassName
+//document.getElementsByClassName("pera").innerHTML = "Mehedi"
+
+//querySelector
+
+document.querySelector("#pid").innerHTML = "this not a peragraph";
+document.querySelector(".pClass").innerHTML = "this is class query selector";
