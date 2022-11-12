e1a3f3de615283815f710d716f3b05e7cb6cd9b4
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-12 09:07:59 +0600

    commited

---
+++ lessons12/script.js
@@ -0,0 +1,9 @@
+var myVar = document.querySelector("#peraId");
+
+function addStyle() {
+    myVar.classList.add("para-style");
+}
+
+function removeStyle() {
+    myVar.classList.remove("para-style");
+}
