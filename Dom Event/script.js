80809e3cdc2a32d4e143eb3e7c62db0687c8e56b
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-15 15:12:58 +0600

    comitted

--- Dom Event/script.js
+++ Dom Event/script.js
@@ -3,29 +3,31 @@
 // <select></select>
 // <textarea></textarea> */}
 
-// const input = document.querySelector("input[name=name")
-// input.addEventListener("change",changeHandeler);
+//code for input type
+const input = document.querySelector("input[name=name")
+input.addEventListener("change",changeHandeler);
 
-// function changeHandeler(event){
-//     console.log(event.target.className);
-//     console.log(event.target.id);
-//     console.log(event.target.value);
+function changeHandeler(event){
+    console.log(event.target.className);
+    console.log(event.target.id);
+    console.log(event.target.value);
 
-// }
+}
 
-// const programes = document.querySelectorAll("input[name=programe]");
-// //console.log(programes);
+//code for checkbox type
+const programes = document.querySelectorAll("input[name=programe]");
+//console.log(programes);
 
-// Array.from(programes).map((programe) =>{
-//     programe.addEventListener("change", programeHandler);
-// });
+Array.from(programes).map((programe) =>{
+    programe.addEventListener("change", programeHandler);
+});
 
-// function programeHandler(e){
-//     if(e.target.checked){
-//         console.log(e.target.value);
-//     }
-// }
-
+function programeHandler(e){
+    if(e.target.checked){
+        console.log(e.target.value);
+    }
+}
+//code for selection type
 const department = document.querySelector("#department");
 //console.log(department);
 
