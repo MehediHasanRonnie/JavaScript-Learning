24c35fc8ff3c82088cfad6767477afd06821c7a4
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-20 11:57:30 +0600

    comitted

---
+++ ES6/variable&fuction/script.js
@@ -0,0 +1,25 @@
+//ES6
+//variable declaration
+// let x = 10;
+// let y = 20;
+
+// let sum = x + y;
+
+// console.log(`the sum is ${sum} ,result shown`);
+
+//string concatination
+
+// let name = "Mehedi Hasan";
+
+// let messege = `I am ${name} and i'm learning javascript`;
+
+// console.log(messege);
+
+//function declaration
+
+
+const add = (x,y) => {
+    let sum = x+y;
+    console.log(sum);
+}
+add(20,30);
