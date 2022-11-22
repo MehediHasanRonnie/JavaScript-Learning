07749bc45b430fce0aa02bb4380f0c66c4039302
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-22 21:15:13 +0600

    commited

---
+++ ES6/stringmethod.js
@@ -0,0 +1,13 @@
+//startsWith(searchString,position)
+
+const message = "Today is sunday";
+
+console.log(message.startsWith("Today",0));
+
+//endsWith(searchString,length)
+
+console.log(message.endsWith("sunday"));
+
+//includes
+console.log(message.includes("or"));
+
