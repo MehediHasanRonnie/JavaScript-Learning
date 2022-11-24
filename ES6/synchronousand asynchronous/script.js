763c18b4563c74d66c6d1609979f8fa79d2aca39
Mehedi Hasan <72302078+MehediHasanRonnie@users.noreply.github.com>
2022-11-24 10:17:34 +0600

    commited

--- ES6/synchronousand asynchronous/script.js
+++ ES6/synchronousand asynchronous/script.js
@@ -5,6 +5,13 @@ const taskOne = () => console.log( "Task1");
 const dataLoading = () => console.log("Task2,data is loading");
 const taskTwo = () => {setTimeout(dataLoading, 2000)}
 
+//Asynchronous usuing annonymous function
+const taskSix = () => {
+    setTimeout(() => {
+        console.log("task 6 data looded");
+    }, 3000);
+}
+
 const taskThree = () => console.log("Task3");
 const taskFour = () => console.log("Task4");
 
@@ -12,3 +19,4 @@ taskOne();
 taskTwo();
 taskThree();
 taskFour();
+taskSix();
